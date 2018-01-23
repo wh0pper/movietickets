7e7c06f32220e1afabeabef4d4bd4a1086bb43d0
Hannah Lee and Jared Clemmensen <hannah@>
2018-01-23 15:48:39 -0800

    add modal and correct price function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,57 @@
 //BUSINNESS LOGIC
-function desires(film, time, age) {
-  this.film = film;
-  this.time = time;
-  this.age = age;
-}
-
 var seniorPrice = 8;
 var seniorMatineePrice = 5;
 var regularPrice = 12;
 var studentPrice = 10;
 var matineePrice = 7;
 
-desires.prototype.price = function() {
-  if (age > 62) {
-    if (time === "matinee") {
-      return seniorMatineePrice;
+function newDesire(film, time, age, price) {
+  this.film = film;
+  this.time = time;
+  this.age = age;
+  this.price = price;
+}
+
+newDesire.prototype.pricer = function() {
+  if (this.age > 62) {
+    if (this.time === "matinee") {
+      this.price = seniorMatineePrice;
     } else {
-      return seniorPrice;
+      this.price = seniorPrice;
     }
-  } else if (age < 18) {
-      if (time === "matinee") {
-        return matineePrice;
+  } else if (this.age < 18) {
+      if (this.time === "matinee") {
+        this.price = matineePrice;
       } else {
-        return studentPrice;
+        this.price = studentPrice;
       }
   } else {
-    if (time === "matinee") {
-      return matineePrice;
-    } return regularPrice;
+    if (this.time === "matinee") {
+      this.price = matineePrice;
+    }
+    else {
+      this.price = regularPrice;
+    }
   }
 }
+
+var desire = {};
+
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var film = $("select#films").val();
+    var time = $("input:radio[name=time]:checked").val();
+    var age = $("input#age").val();
+
+    desire = new newDesire(film, time, age);
+    desire.pricer();
+
+    // $("#modalCenter").on("shown.bs.modal", function() {
+    //   $(".modal-body p").trigger('focus')
+    // })
+    $(".modal-body").text(desire.price);
+    $('#modalCenter').modal('show');
+  });
+});
